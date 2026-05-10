80a7a1d3a83abd0d1e77d4d5fdfcde2e64d50b01
Mateusz <01206777@pw.edu.pl>
2026-05-10 16:14:15 +0200

    Implementacja cords.java

--- src/Cords.java
+++ src/Cords.java
@@ -5,4 +5,36 @@ public class Cords {
     zwracanie wsp dla danego wierzchołka
     oraz dodawanie wsp dla danego wierzchołka
      */
+    private int n;
+    private final double[] xs;
+    private final double[] ys;
+
+    public Cords(int n){
+        this.n = n;
+        this.xs = new double[n+1];
+        this.ys = new double[n+1];
+    }
+    public void set(int v, double x, double y){
+        if(v>=1 && v<=n){
+            xs[v] = x;
+            ys[v] = y;
+        }
+    }
+    public double getX(int v){
+        if(v>=1 && v<=n)return xs[v];
+        return 0.0;
+    }
+    public double getY(int v){
+        if(v>=1 && v<=n) return ys[v];
+        return 0.0;
+    }
+    public void add(int v, double deltaX, double deltaY){
+        if(v>=1 && v<=n){
+        xs[v] += deltaX;
+        ys[v] +=deltaY;
+        }
+    }
+    public int getN(){
+        return n;
+    }
 }
